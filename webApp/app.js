f5402c758044f66ba619ec04a57813f9f4e065d5
Samer AL Masri <almasri@ualberta.ca>
2020-01-31 17:50:16 -0500

    Updated webApp to fetch tripIDs from database and put them in the main html page through ajax
    Signed-off-by: Samer AL Masri <almasri@ualberta.ca>

--- webApp/app.js
+++ webApp/app.js
@@ -1,15 +1,24 @@
 const express = require("express");
+const http = require('http');
+var mysql      = require('mysql');
+
 const app = express();
 const PORT = 8080; // default port 8080
-
-const http = require('http');
-
 const hostname = '0.0.0.0'
 const port = 8080;
 
 app.set("view engine", "ejs");
 
 app.use(express.static('static')); // Tell NodeJS where to find the JS/CSS files
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'samasri2',
+  password : '1',
+  database : 'yallaDb'
+});
+ 
+connection.connect();
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -18,8 +27,31 @@ const server = http.createServer((req, res) => {
 });
 
 app.get("/", (req, res) => {
-  res.render("login", {"user": "user"});
+  connection.query("SELECT DISTINCT(tripID) FROM trips", function (error, results, fields) {
+    results = results.slice(0,5);
+    tripIDs = []
+    for(var r of results) tripIDs.push(r.tripID);
+    res.render("login", {"tripIDs": tripIDs});
+  });
 });
+
+app.get("/trips", (req,res) => {
+  connection.query("SELECT DISTINCT(tripID) FROM trips", function (error, results, fields) {
+    results = results.slice(0,5);
+    tripIDs = []
+    for(var r of results) tripIDs.push(r.tripID);
+    res.end(tripIDs.toString());
+  });
+});
+
+// app.get("/trip/:tripID", (req,res) => {
+//   connection.query("SELECT DISTINCT(tripID) FROM trips", function (error, results, fields) {
+//     results = results.slice(0,5);
+//     tripIDs = []
+//     for(var r of results) tripIDs.push(r.tripID);
+//     res.end(tripIDs.toString());
+//   });
+// });
 
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
--- webApp/static/test.js
+++ webApp/static/test.js
@@ -1,6 +1,4 @@
-// import {token} from "./token.js";
-
-$( document ).ready(function() {
+$(document).ready(function() {
     var mymap = L.map('mapid').setView([51.505, -0.09], 13);
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -8,5 +6,36 @@ $( document ).ready(function() {
         id: 'mapbox/streets-v11',
         accessToken: token
     }).addTo(mymap);
+
+    $.ajax({
+        type: 'GET',
+        url: '/trips', 
+        success: function(r) {
+            let tripIDs = r.split(',');
+            for(tripID of tripIDs) {
+                $('#routes').append($('<option>', {
+                    value: 1,
+                    text: tripID
+                }));
+            }
+        },
+        error: function() {
+          alert("Could not successfully reach server/trips")
+        }
+    });
+
+    //listen for 'submit'
+    $('.container').find('form').on('submit', function(event) {
+        return false;
+        // $.ajax({
+        //     type: 'POST',
+        //     url: '/tweets', 
+        //     "data": $textbox,
+        //     success: function() {
+        //     $('main #tweetSection').html("")    // clear all html
+        //     loadTweets()}                       // load tweets again
+        //     // error: function() {     //sometimes problematic... remove?
+        //     //   alert("Could not load tweets")}
+        // })
+    });
 });
-        
