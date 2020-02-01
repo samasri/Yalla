42f369cefbf5168f238bf59e7140aedd4ec2f43a
Samer AL Masri <almasri@ualberta.ca>
2020-02-01 16:39:57 -0500

    Changed script paths to relative
    Signed-off-by: Samer AL Masri <almasri@ualberta.ca>

--- run.sh
+++ run.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+
 if [ "$1" == "stop" ]; then
     kill -9 $(pgrep python3)
 else 
     while true; do
        DATE=$(date '+%F')
-       touch /home/samasri/yalla/results/$DATE /home/samasri/yalla/results-err/$DATE
-      /home/samasri/yalla/getDistances.py 60 >> /home/samasri/yalla/results/$DATE 2>> /home/samasri/yalla/results-err/$DATE
+       mkdir -p results-err results
+       touch results/$DATE results-err/$DATE
+       ./getDistances.py 60 >> results/$DATE 2>> results-err/$DATE
     done
 fi
